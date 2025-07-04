2e3ded1f860bea98c6cfc726fea4ee9e4958a2b7
Julian Sampels <julian.sampels@campus.tu-berlin.de>
2025-07-04 03:19:36 +0200

    add create_and_run_training_exp_tuples for training on tuples; work in progess, not usable at the moment!

--- PathE/pathe/runner.py
+++ PathE/pathe/runner.py
@@ -211,7 +211,7 @@ def main():
 
     # print(args)
     if args.model == "pathe":
-        pathe_trainer.create_and_run_training_exp(args)
+        pathe_trainer.create_and_run_training_exp_triples(args)
 
 
 
